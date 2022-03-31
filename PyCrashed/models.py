Fixed class weighting in multi-out model

PyCrashed/models.py
@@ -67,12 +67,13 @@ class Model():
     def build(self):
         # Builds a model according to the inputs and outputs specified
         i, o = self.specify_model()
-        self.model = tf.keras.Model(inputs=i, outputs=o, name="nvidia")
+        self.model = tf.keras.Model(inputs=i, outputs=o, name=self.name)
         self.model.compile(
             optimizer=self.optimizer,
             loss=self.loss,
             metrics=self.metrics,
         )
+        self.is_split = isinstance(o, (list, tuple))
         return self.model
 
     def fit(self,
@@ -80,8 +81,8 @@ class Model():
             validation_data: tf.data.Dataset = None,
             n_epochs: int = 10
         ):
-        if not data:
-            data = Dataset.load("train")
+        # if not data:
+        #     data = Dataset.load("train")
         if not validation_data:
             validation_data = Dataset.load("val")
 
@@ -175,8 +176,8 @@ class NVidiaSplit(Model):
         kwargs["activation"] = kwargs.get('activation', "elu")
         super().__init__("Nvidia_split", **kwargs)
         self.loss = {
-            "angle": tf.losses.MeanSquaredError(),
-            "speed": tf.losses.BinaryCrossentropy()
+            "angle": tf.keras.losses.MeanSquaredError(),
+            "speed": tf.keras.losses.BinaryCrossentropy()
         }
         self.metrics = {
             "angle": tf.keras.metrics.RootMeanSquaredError(),

PyCrashed/pipeline.py
@@ -32,11 +32,19 @@ def get_set(valid_l, prob, prop, n):
     take_items = lambda c: valid_l[valid_l["motion_class"] == c].sample(int(n * prob * prop.loc[c]))
     return pd.concat(map(take_items, valid_l["motion_class"].unique().squeeze()))
 
+def process_out(img, label, weights):
+    l = {
+        "angle": label[0],
+        "speed": label[1]
+    }
+    return img, l, tf.reshape(weights, (1, -1))
+
 class Dataset:
     n_train = 0.65
     n_val = 0.25
     n_test = 0.1
     batch_size = 64
+    labelled_outputs = False
 
     # Define the image paths
     labels = assign_weights(pd.read_csv("data/training_norm.csv"))
@@ -107,11 +115,14 @@ class Dataset:
         f_test  = list(filter(lambda x: x in idx_test, Dataset.paths))
 
         def build(files):
-            return tf.data.Dataset.list_files(files)\
-                .map(tf_fetch_data, num_parallel_calls=tf.data.AUTOTUNE, deterministic=False)\
-                .batch(Dataset.batch_size)\
-                .cache()\
-                .prefetch(tf.data.AUTOTUNE)
+            ds = tf.data.Dataset.from_tensor_slices(files)
+            ds = ds.map(tf_fetch_data, num_parallel_calls=tf.data.AUTOTUNE, deterministic=False)
+            if Dataset.labelled_outputs:
+                ds = ds.map(process_out, num_parallel_calls=tf.data.AUTOTUNE, deterministic=False)
+            ds = ds.batch(Dataset.batch_size)
+            ds = ds.cache()
+            ds = ds.prefetch(tf.data.AUTOTUNE)
+            return ds
 
         if mode == "train":
             return build(f_train)
@@ -132,4 +143,6 @@ class Dataset:
 
         ds = tf.data.Dataset.list_files(Dataset.prediction_paths)
         ds = ds.map(tf_fetch_data, deterministic=True, num_parallel_calls=tf.data.AUTOTUNE)
+        if Dataset.labelled_outputs:
+            ds = ds.map(process_out, num_parallel_calls=tf.data.AUTOTUNE, deterministic=False)
         return ds.batch(1).cache().prefetch(tf.data.AUTOTUNE)

PyCrashed/utils.py
@@ -54,14 +54,6 @@ def train_model(args):
     gpus = tf.config.list_logical_devices('GPU')
     strategy = tf.distribute.MirroredStrategy(gpus)
 
-    printf("Configuring data pipeline... ", end="")
-    Dataset.n_train = args.train
-    Dataset.n_val = args.val
-    Dataset.n_test = 1 - (args.train + args.val)
-    Dataset.batch_size = strategy.num_replicas_in_sync * args.batch
-    ds = Dataset.load("train")
-    printf("Done!")
-
     # Compile the model within the scope
     printf("Building model... ", end="")
     printf("Instantiating model... ", end="")
@@ -86,9 +78,14 @@ def train_model(args):
         if args.restore: model.restore()
     printf("Done!")
 
-    fmt = lambda k: k.replace('_', ' ').title()
-    printf("Executing model using the following dataset configuration")
-    # printf(tabulate({fmt(k): [v] for k, v in Dataset._props.items()}, headers="keys", tablefmt="fancy_grid"))
+    printf("Configuring data pipeline... ", end="")
+    Dataset.n_train = args.train
+    Dataset.n_val = args.val
+    Dataset.n_test = 1 - (args.train + args.val)
+    Dataset.batch_size = strategy.num_replicas_in_sync * args.batch
+    Dataset.labelled_outputs = model.is_split
+    ds = Dataset.load("train")
+    printf("Done!")
 
     printf("Training model")
     model.fit(n_epochs=args.epochs, data=ds)

scratch.py
@@ -1,17 +1,44 @@
 # %%
 from PyCrashed.pipeline import Dataset
-# %%
+Dataset.batch_size=4
+Dataset.labelled_outputs = True
 ds = Dataset.load("train")
 # %%
-len(list(ds))
+lab = next(iter(ds.take(1)))[1]
+# %%
+lab
+# %%
+Dataset.labelled_outputs
+# %%
+from PyCrashed.models import NVidiaSplit
+x = NVidiaSplit(use_wandb=False)
+x.build()
+x.is_split
+# %%
 
+from PyCrashed.models import NVidia
+x = NVidia(use_wandb=False)
+x.build()
+x.is_split
 # %%
-ds2 = Dataset.load("val")
-len(list(ds2))
+import tensorflow as tf
+import numpy as np
 # %%
-i = i[0, :, :, :]
+mse = tf.keras.losses.MeanSquaredError()
 # %%
-import matplotlib.pyplot as plt
+y = np.array([1, 0, 0, 1], dtype=float)
+y_pred = np.array([1, 1, 0, 0], dtype=float)
+w = np.array([0.8, 2, 0.2, 0.2]).reshape(1, -1)
 # %%
-plt.imshow(i)
+w.shape
+# %%
+y = tf.convert_to_tensor(y)
+y_pred = tf.convert_to_tensor(y_pred)
+w = tf.convert_to_tensor(w)
+# %%
+mse(y, y_pred)
+# %%
+mse(lab["angle"], lab["angle"], [0.8, 2, 0.2, 0.2]).numpy()
+# %%
+lab["angle"].reshape
 # %%